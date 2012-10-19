d46dba36d5c08681ee3f924cfdadef7ba2d16463
Michael Bisbjerg <michael@mbwarez.dk>
2012-10-19 03:59:56 +0200

    Moved common enums out of DiskIO objects

--- DeviceIOControlLib/DeviceIODisk.cs
+++ DeviceIOControlLib/DeviceIODisk.cs
@@ -3,24 +3,6 @@ using System.Runtime.InteropServices;
 
 namespace DeviceIOControlLib
 {
-    public enum PartitionStyle
-    {
-        PARTITION_STYLE_MBR = 0,
-        PARTITION_STYLE_GPT = 1,
-        PARTITION_STYLE_RAW = 2
-    }
-
-    [Flags]
-    public enum EFIPartitionAttributes : ulong
-    {
-        GPT_ATTRIBUTE_PLATFORM_REQUIRED = 0x0000000000000001,
-        LegacyBIOSBootable = 0x0000000000000004,
-        GPT_BASIC_DATA_ATTRIBUTE_NO_DRIVE_LETTER = 0x8000000000000000,
-        GPT_BASIC_DATA_ATTRIBUTE_HIDDEN = 0x4000000000000000,
-        GPT_BASIC_DATA_ATTRIBUTE_SHADOW_COPY = 0x2000000000000000,
-        GPT_BASIC_DATA_ATTRIBUTE_READ_ONLY = 0x1000000000000000
-    }
-
     public enum MEDIA_TYPE : uint
     {
         Unknown,
--- DeviceIOControlLib/Enums.cs
+++ DeviceIOControlLib/Enums.cs
@@ -83,4 +83,22 @@ namespace DeviceIOControlLib
         OutDirect = 2,
         Neither = 3
     }
+
+    public enum PartitionStyle
+    {
+        PARTITION_STYLE_MBR = 0,
+        PARTITION_STYLE_GPT = 1,
+        PARTITION_STYLE_RAW = 2
+    }
+
+    [Flags]
+    public enum EFIPartitionAttributes : ulong
+    {
+        GPT_ATTRIBUTE_PLATFORM_REQUIRED = 0x0000000000000001,
+        LegacyBIOSBootable = 0x0000000000000004,
+        GPT_BASIC_DATA_ATTRIBUTE_NO_DRIVE_LETTER = 0x8000000000000000,
+        GPT_BASIC_DATA_ATTRIBUTE_HIDDEN = 0x4000000000000000,
+        GPT_BASIC_DATA_ATTRIBUTE_SHADOW_COPY = 0x2000000000000000,
+        GPT_BASIC_DATA_ATTRIBUTE_READ_ONLY = 0x1000000000000000
+    }
 }
